806f3ef4a5acaddfbe91a3cd1434024da4a21698
Rich <richbport@yahoo.com>
2025-09-22 13:10:58 -0400

    setCache

---
+++ Caching Example/script.js
@@ -0,0 +1,8 @@
+// Function to store data in localStorage with TTL (Time-T0-Live)
+function setCache(key, value, ttl) {
+  const item = {
+    value: value,
+    expiry: Date.now() + ttl, // Set expiry time
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+}
