9f13fe0a69b44ef769d27eaa59d935891e8402ad
Rich <richbport@yahoo.com>
2025-09-23 14:51:27 -0400

    Date.now() > item.expiry

--- Caching Example/script.js
+++ Caching Example/script.js
@@ -1,34 +1,25 @@
 // Function to store data in localStorage with TTL
-// function setCache(key, value, ttl) {
-//     const item = {
-//         value: value,
-//         expiry: Date.now() + ttl, // Set expiry time
-//     };
-//     localStorage.setItem(key, JSON.stringify(item));
-// }
-
-// Function to retrieve data from localStorage if ttl hasn't expired
-// function getCache(key) {
-//     const itemStr = localStorage.getItem(key);
-//     if (!itemStr) {
-//         return null;
-//     }
-
-// const item = JSON.parse(itemStr);
-
-// // Check if the cache item is expired
-// if (Date.now() > item.expiry) {
-//     localStorage.removeItem(key);
-//     return null;
-// }
-// return item.value;
-// }
-
-function getCache(key) {
-  const itemStr = localStorage.getItem(key);
-  if (!itemStr) {
-    return null;
-  }
+function setCache(key, value, ttl) {
+    const item = {
+        value: value,
+        expiry: Date.now() + ttl, // Set expiry time
+    };
+    localStorage.setItem(key, JSON.stringify(item));
 }
 
+// Function to retrieve data from localStorage if ttl hasn't expired
+function getCache(key) {
+    const itemStr = localStorage.getItem(key);
+    if (!itemStr) {
+        return null;
+    }
+
 const item = JSON.parse(itemStr);
+
+// // Check if the cache item is expired
+if (Date.now() > item.expiry) {
+    localStorage.removeItem(key);
+    return null;
+}
+return item.value;
+}
