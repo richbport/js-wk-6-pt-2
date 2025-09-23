9243580e0983203c186eae82176325e2e287697a
Rich <richbport@yahoo.com>
2025-09-23 19:01:08 -0400

    cachedExpiry

--- Combined Practical Example/script.js
+++ Combined Practical Example/script.js
@@ -1,1 +1,21 @@
 const API_URL = "https://jsonplaceholder.typicode.com/posts/1";
+
+async function fetchData() {
+    try {
+
+       const cachedData = localStorage.getItem("cachedPost");    
+    } catch {
+
+    }
+}
+
+async function fetchData() {
+    try {
+
+       const cachedData = localStorage.getItem("cachedPost");    
+       const cachedTime = localStorage.getItem("cachedTime");
+
+       const cachedExpiry = 
+
+    } catch {}
+}
